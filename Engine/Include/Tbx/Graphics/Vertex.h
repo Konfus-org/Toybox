5e7b08fe8830b64b87e12d17a0c93c0728a50c2e
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-09-06 14:54:47 -0700

    Fix cross-platform build issues

--- Engine/Include/Tbx/Graphics/Vertex.h
+++ Engine/Include/Tbx/Graphics/Vertex.h
@@ -11,15 +11,15 @@ namespace Tbx
         explicit(false) Vertex(const Vector3& position) 
             : Position(position) {}
         Vertex(const Vector3& position, const Color& color)
-            : Position(position), Color(color) {}
+            : Position(position), Tint(color) {}
         Vertex(const Vector3& position, const Vector3& normal, const Vector2I& texCoord)
             : Position(position), Normal(normal), TexCoord(texCoord) {}
         Vertex(const Vector3& position, const Vector3& normal, const Vector2I& texCoord, const Color& color)
-            : Position(position), Normal(normal), TexCoord(texCoord), Color(color) {}
+            : Position(position), Normal(normal), TexCoord(texCoord), Tint(color) {}
 
         Vector3 Position  = { 0, 0, 0 };                // (x, y, z) in 3D space
         Vector3 Normal    = { 0, 0, 0 };                // (nx, ny, nz) for lighting
         Vector2I TexCoord = { 0, 0 };                   // (u, v) for texture mapping
-        Color Color       = { 1.0f, 1.0f, 1.0f, 1.0f }; // (r, g, b, a) for color
+        Color Tint        = { 1.0f, 1.0f, 1.0f, 1.0f }; // (r, g, b, a) for color
     };
 }
