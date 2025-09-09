37d5ffa4c0994331790e7af85951ed59e0beb153
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-09-09 13:15:11 -0700

    Use smart parent pointers in Toy hierarchy

--- Engine/Include/Tbx/TBS/World.h
+++ Engine/Include/Tbx/TBS/World.h
@@ -1,13 +1,128 @@
 #pragma once
 #include "Tbx/DllExport.h"
+#include "Tbx/TBS/Toy.h"
+#include <functional>
 #include <memory>
+#include <utility>
+#include <vector>
 
 namespace Tbx
 {
+    /// <summary>
+    /// Provides iteration over toys that contain data of type <typeparamref name="T"/>.
+    /// </summary>
+    template <typename T>
+    class WorldView
+    {
+    public:
+        using iterator = typename std::vector<std::shared_ptr<Toy>>::iterator;
+        using const_iterator = typename std::vector<std::shared_ptr<Toy>>::const_iterator;
+
+        /// <summary>
+        /// Creates a view rooted at the given toy.
+        /// </summary>
+        /// <param name="root">Root toy to search from.</param>
+        explicit WorldView(const std::shared_ptr<Toy>& root)
+        {
+            if (root)
+            {
+                Build(root);
+            }
+        }
+
+        /// <summary>
+        /// Returns an iterator to the first toy in the view.
+        /// </summary>
+        iterator begin()
+        {
+            return _toys.begin();
+        }
+
+        /// <summary>
+        /// Returns an iterator one past the last toy in the view.
+        /// </summary>
+        iterator end()
+        {
+            return _toys.end();
+        }
+
+        /// <summary>
+        /// Returns a const iterator to the first toy in the view.
+        /// </summary>
+        const_iterator begin() const
+        {
+            return _toys.begin();
+        }
+
+        /// <summary>
+        /// Returns a const iterator one past the last toy in the view.
+        /// </summary>
+        const_iterator end() const
+        {
+            return _toys.end();
+        }
+
+    private:
+        void Build(const std::shared_ptr<Toy>& toy)
+        {
+            if (toy->template HasBlock<T>())
+            {
+                _toys.push_back(toy);
+            }
+            for (const auto& child : toy->GetChildren())
+            {
+                Build(child);
+            }
+        }
+
+        std::vector<std::shared_ptr<Toy>> _toys = {};
+    };
+
+    /// <summary>
+    /// Function invoked for systems that operate on toys containing data of type <typeparamref name="T"/>.
+    /// </summary>
+    template <typename T>
+    using WorldSystem = std::function<void(WorldView<T>)>;
+
+    /// <summary>
+    /// Manages a hierarchy of toys and executes registered systems.
+    /// </summary>
     class World
     {
     public:
+        /// <summary>
+        /// Initializes a new instance of <see cref="World"/> with a root toy.
+        /// </summary>
+        EXPORT World();
+
+        /// <summary>
+        /// Gets the root toy of the hierarchy.
+        /// </summary>
+        /// <returns>The root toy.</returns>
+        EXPORT std::shared_ptr<Toy> GetRoot() const;
+
+        /// <summary>
+        /// Registers a system to operate on toys containing data of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">Type of data required by the system.</typeparam>
+        /// <param name="system">The system callback to register.</param>
+        template <typename T>
+        void AddSystem(WorldSystem<T> system)
+        {
+            _systems.push_back([this, system = std::move(system)]()
+            {
+                system(WorldView<T>(_root));
+            });
+        }
+
+        /// <summary>
+        /// Updates all registered systems and then updates the toy hierarchy.
+        /// </summary>
         EXPORT void Update();
-        EXPORT void Destroy();
+
+    private:
+        std::shared_ptr<Toy> _root = {};
+        std::vector<std::function<void()>> _systems = {};
     };
 }
+
--- Engine/Source/Tbx/TBS/Toy.cpp
+++ Engine/Source/Tbx/TBS/Toy.cpp
@@ -1,12 +1,104 @@
 #include "Tbx/PCH.h"
 #include "Tbx/TBS/Toy.h"
+#include <algorithm>
 
 namespace Tbx
 {
-    std::unordered_map<hash, uint32> BlockTypeIndexProvider::_blockTypeTable = {};
-
-    std::unordered_map<hash, uint32>& BlockTypeIndexProvider::GetMap()
+    Toy::Toy(const std::string& name) :
+        _handle(name)
     {
-        return _blockTypeTable;
+    }
+
+    void Toy::AddChild(const std::shared_ptr<Toy>& child)
+    {
+        if (child)
+        {
+            child->_parent = shared_from_this();
+            _children.push_back(child);
+        }
+    }
+
+    void Toy::RemoveChild(const std::shared_ptr<Toy>& child)
+    {
+        auto it = std::find(_children.begin(), _children.end(), child);
+        if (it != _children.end())
+        {
+            (*it)->_parent.reset();
+            _children.erase(it);
+        }
+    }
+
+    std::shared_ptr<Toy> Toy::GetChild(const ToyHandle& handle) const
+    {
+        for (const auto& child : _children)
+        {
+            if (child->GetHandle() == handle)
+            {
+                return child;
+            }
+        }
+        return {};
+    }
+
+    std::shared_ptr<Toy> Toy::FindChild(std::string_view name) const
+    {
+        for (const auto& child : _children)
+        {
+            if (child->GetName() == name)
+            {
+                return child;
+            }
+        }
+        return {};
+    }
+
+    const std::vector<std::shared_ptr<Toy>>& Toy::GetChildren() const
+    {
+        return _children;
+    }
+
+    void Toy::SetParent(const std::shared_ptr<Toy>& parent)
+    {
+        _parent = parent;
+    }
+
+    std::shared_ptr<Toy> Toy::GetParent() const
+    {
+        return _parent.lock();
+    }
+
+    const std::string& Toy::GetName() const
+    {
+        return _handle.GetName();
+    }
+
+    const ToyHandle& Toy::GetHandle() const
+    {
+        return _handle;
+    }
+
+    void Toy::SetEnabled(bool enabled)
+    {
+        _enabled = enabled;
+    }
+
+    bool Toy::IsEnabled() const
+    {
+        return _enabled;
+    }
+
+    void Toy::Update()
+    {
+        if (!_enabled)
+        {
+            return;
+        }
+
+        OnUpdate();
+        for (auto& child : _children)
+        {
+            child->Update();
+        }
     }
 }
+
