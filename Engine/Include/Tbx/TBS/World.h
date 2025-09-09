75d97944b6d867248cc9fa0838bd294c50685c12
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-09-09 10:12:14 -0700

    Removing old versions of toy and removing box as well.

--- Engine/Include/Tbx/TBS/World.h
+++ Engine/Include/Tbx/TBS/World.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "Tbx/DllExport.h"
-#include "Tbx/TBS/Box.h"
 #include <memory>
 
 namespace Tbx
@@ -8,20 +7,7 @@ namespace Tbx
     class World
     {
     public:
-        EXPORT static void SetContext();
-        EXPORT static void Update();
-        EXPORT static void Destroy();
-
-        EXPORT static Uid MakeBox();
-        EXPORT static void RemoveBox(Uid id);
-
-        EXPORT static std::shared_ptr<Box> GetBox(Uid id);
-        EXPORT static std::vector<std::shared_ptr<Box>> GetBoxes();
-        EXPORT static uint32 GetBoxCount();
-
-    private:
-        // TODO: make a pool of boxes
-        static std::vector<std::shared_ptr<Box>> _boxes;
-        static uint32 _boxCount;
+        EXPORT void Update();
+        EXPORT void Destroy();
     };
 }
