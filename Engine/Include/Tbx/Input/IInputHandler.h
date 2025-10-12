0caa26cdcbecc50f6571064ecb57aab118554da0
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-10-11 17:04:35 -0700

    Update input refresh method name

--- Engine/Include/Tbx/Input/IInputHandler.h
+++ Engine/Include/Tbx/Input/IInputHandler.h
@@ -9,7 +9,11 @@ namespace Tbx
     public:
         virtual ~IInputHandler() = default;
 
-        virtual void RefreshInputStates() = 0;
+        /// <summary>
+        /// Updates the states of connected input devices.
+        /// Should be called once per frame.
+        /// </summary>
+        virtual void UpdateInputState() = 0;
 
         virtual bool IsGamepadButtonDown(int playerIndex, int button) const = 0;
         virtual bool IsGamepadButtonUp(int playerIndex, int button) const = 0;
