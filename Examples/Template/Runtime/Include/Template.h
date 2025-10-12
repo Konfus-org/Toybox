69751e7a208b780d81f0f9688bb7c769e50f477d
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-10-12 15:54:20 -0700

    Some cleanup to cmake files and fiddling with audio

---
+++ Examples/Template/Runtime/Include/Template.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <Tbx/App/Runtime.h>
+#include <Tbx/Stages/Toy.h>
+#include <Tbx/Stages/Stage.h>
+#include "Tbx/Memory/Refs.h"
+#include <Tbx/Graphics/Material.h>
+#include <Tbx/Math/Vectors.h>
+
+class Demo final : public Tbx::Runtime
+{
+public:
+    Demo(Tbx::Ref<Tbx::EventBus> eventBus);
+    ~Demo();
+
+    void OnStart() override;
+    void OnShutdown() override;
+    void OnUpdate(const Tbx::DeltaTime& deltaTime) override;
+
+private:
+    Tbx::Ref<Tbx::Stage> _stage = nullptr;
+    Tbx::Ref<Tbx::Toy> _fpsCam = nullptr;
+    Tbx::Ref<Tbx::Toy> _smily = nullptr;
+
+    float _smilyBobTime = 0.0f;
+    float _smilyBobAmplitude = 0.0f;
+    float _smilyBaseHeight = 0.0f;
+
+    Tbx::Vector2 _camLookVelocity = Tbx::Vector2::Zero;
+    float _camPitch = 0.0f;
+    float _camYaw = 0.0f;
+};
+
+TBX_REGISTER_RUNTIME(Demo);
