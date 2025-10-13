a71f2b67ba4755ddbfec78737aacee9bbf472871
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-10-12 19:59:22 -0700

    Update EventBus.cpp

--- Engine/Source/Tbx/Events/EventBus.cpp
+++ Engine/Source/Tbx/Events/EventBus.cpp
@@ -1,136 +1,137 @@
 #include "Tbx/PCH.h"
 #include "Tbx/Events/EventBus.h"
-#include "Tbx/Debug/Asserts.h"
-#include "Tbx/Debug/Tracers.h"
 #include "Tbx/Events/EventSync.h"
+#include "Tbx/Debug/Tracers.h"
+#include "Tbx/Debug/IPrintable.h"
+#include <Tbx/Memory/Refs.h>
 #include <unordered_map>
 
 namespace Tbx
 {
-    //////////// Event Suppressor ///////////////
+	//////////// Event Suppressor ///////////////
 
-    std::atomic_int EventSuppressor::_suppressCount = 0;
+	std::atomic_int EventSuppressor::_suppressCount = 0;
 
-    EventSuppressor::EventSuppressor()
-    {
-        Suppress();
-    }
+	EventSuppressor::EventSuppressor()
+	{
+		Suppress();
+	}
 
-    EventSuppressor::~EventSuppressor()
-    {
-        Unsuppress();
-    }
+	EventSuppressor::~EventSuppressor()
+	{
+		Unsuppress();
+	}
 
-    bool EventSuppressor::IsSuppressing()
-    {
-        return _suppressCount.load(std::memory_order_relaxed) > 0;
-    }
+	bool EventSuppressor::IsSuppressing()
+	{
+		return _suppressCount.load(std::memory_order_relaxed) > 0;
+	}
 
-    void EventSuppressor::Suppress()
-    {
-        _suppressCount.fetch_add(1, std::memory_order_relaxed);
-    }
+	void EventSuppressor::Suppress()
+	{
+		_suppressCount.fetch_add(1, std::memory_order_relaxed);
+	}
 
-    void EventSuppressor::Unsuppress()
-    {
-        _suppressCount.fetch_sub(1, std::memory_order_relaxed);
-    }
+	void EventSuppressor::Unsuppress()
+	{
+		_suppressCount.fetch_sub(1, std::memory_order_relaxed);
+	}
 
-    //////////// Event Bus ///////////////
+	//////////// Event Bus ///////////////
 
-    Ref<EventBus> EventBus::Global = CreateGlobal();
-    bool EventBus::_creatingGlobal = false;
+	Ref<EventBus> EventBus::Global = CreateGlobal();
+	bool EventBus::_creatingGlobal = false;
 
-    EventBus::EventBus(Ref<EventBus> parent)
-        : Parent(parent != nullptr && !_creatingGlobal
-            ? parent
-            : Global)
-    {
-    }
+	EventBus::EventBus(Ref<EventBus> parent)
+		: Parent(parent != nullptr && !_creatingGlobal
+			? parent
+			: Global)
+	{
+	}
 
-    EventBus::~EventBus()
-    {
-        EventSync sync;
-        while (!EventQueue.empty())
-        {
-            EventQueue.pop();
-        }
-        Subscriptions.clear();
-        SubscriptionIndex.clear();
-    }
+	EventBus::~EventBus()
+	{
+		EventSync sync;
+		while (!EventQueue.empty())
+		{
+			EventQueue.pop();
+		}
+		Subscriptions.clear();
+		SubscriptionIndex.clear();
+	}
 
-    void EventBus::Flush()
-    {
-        std::queue<ExclusiveRef<Event>> localQueue;
-        {
-            EventSync sync;
-            localQueue.swap(EventQueue);
-        }
+	void EventBus::Flush()
+	{
+		std::queue<ExclusiveRef<Event>> localQueue;
+		{
+			EventSync sync;
+			localQueue.swap(EventQueue);
+		}
 
-        while (!localQueue.empty())
-        {
-            auto event = std::move(localQueue.front());
-            localQueue.pop();
+		while (!localQueue.empty())
+		{
+			auto event = std::move(localQueue.front());
+			localQueue.pop();
 
-            if (!event)
-            {
-                continue;
-            }
+			if (!event)
+			{
+				continue;
+			}
 
-            // If suppressed globally, skip processing of queued events as well.
-            if (EventSuppressor::IsSuppressing())
-            {
-                TBX_TRACE_WARNING("EventBus: Queued event \"{}\" suppressed", event->ToString());
-                continue;
-            }
+			// If suppressed globally, skip processing of queued events as well.
+			if (EventSuppressor::IsSuppressing())
+			{
+				TBX_TRACE_WARNING("EventBus: Queued event \"{}\" suppressed", event->ToString());
+				continue;
+			}
 
-            std::unordered_map<Uid, EventCallback> callbacks;
-            const auto hashCode = Memory::Hash(*event);
-            CollectCallbacks(hashCode, callbacks);
-            if (callbacks.empty())
-            {
-                continue;
-            }
+			std::unordered_map<Uid, EventCallback> callbacks;
+			const auto hashCode = Memory::Hash(*event);
+			CollectCallbacks(hashCode, callbacks);
+			if (callbacks.empty())
+			{
+				continue;
+			}
 
-            for (auto& [id, callback] : callbacks)
-            {
-                if (EventSuppressor::IsSuppressing())
-                {
-                    TBX_TRACE_WARNING("EventBus: The event \"{}\" is suppressed during flush", event->ToString());
-                    break;
-                }
+			for (auto& [id, callback] : callbacks)
+			{
+				if (EventSuppressor::IsSuppressing())
+				{
+					TBX_TRACE_WARNING("EventBus: The event \"{}\" is suppressed during flush", event->ToString());
+					break;
+				}
 
-                callback(*event);
-            }
-        }
-    }
+				callback(*event);
+			}
+		}
+	}
 
-    Ref<EventBus> EventBus::CreateGlobal()
-    {
-        _creatingGlobal = true;
-        auto bus = MakeRef<EventBus>();
-        _creatingGlobal = false;
-        return bus;
-    }
+	Ref<EventBus> EventBus::CreateGlobal()
+	{
+		_creatingGlobal = true;
+		auto bus = MakeRef<EventBus>();
+		_creatingGlobal = false;
+		return bus;
+	}
 
-    void EventBus::CollectCallbacks(EventHash eventKey, std::unordered_map<Uid, EventCallback>& callbacks) const
-    {
-        Ref<EventBus> parentCopy = nullptr;
+	void EventBus::CollectCallbacks(EventHash eventKey, std::unordered_map<Uid, EventCallback>& callbacks) const
+	{
+		Ref<EventBus> parentCopy = nullptr;
 
-        {
-            EventSync sync;
-            auto it = Subscriptions.find(eventKey);
-            if (it != Subscriptions.end())
-            {
-                callbacks.insert(it->second.begin(), it->second.end());
-            }
+		{
+			EventSync sync;
+			auto it = Subscriptions.find(eventKey);
+			if (it != Subscriptions.end())
+			{
+				callbacks.insert(it->second.begin(), it->second.end());
+			}
 
-            parentCopy = Parent;
-        }
+			parentCopy = Parent;
+		}
 
-        if (parentCopy)
-        {
-            parentCopy->CollectCallbacks(eventKey, callbacks);
-        }
-    }
+		if (parentCopy)
+		{
+			parentCopy->CollectCallbacks(eventKey, callbacks);
+		}
+	}
 }
