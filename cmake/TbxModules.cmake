2b18705a562f0e4bc85c9c40f16c6f359702f2d7
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2026-01-23 10:27:37 -0800

    Defer plugin registration via target properties

--- cmake/TbxModules.cmake
+++ cmake/TbxModules.cmake
@@ -1,42 +1,58 @@
 include_guard(GLOBAL)
 
-function(tbx_add_module target_name)
+function(tbx_register_module target_name)
     if(NOT target_name)
-        message(FATAL_ERROR "tbx_add_module: target name is required")
+        message(FATAL_ERROR "tbx_register_module: target name is required")
     endif()
 
-    if(TARGET ${target_name})
-        message(FATAL_ERROR "tbx_add_module: target '${target_name}' already exists")
+    if(NOT TARGET ${target_name})
+        if(TBX_BUILD_SHARED)
+            add_library(${target_name} SHARED)
+            target_compile_definitions(${target_name}
+                PUBLIC
+                    TBX_SHARED_LIB
+                PRIVATE
+                    TBX_EXPORTING_SYMBOLS
+            )
+        else()
+            add_library(${target_name} STATIC)
+        endif()
+
+        target_precompile_headers(${target_name} PRIVATE "${PROJECT_SOURCE_DIR}/modules/common/include/tbx/pch.h")
+
+        if(WIN32)
+            target_compile_definitions(${target_name}
+                PUBLIC
+                    TBX_PLATFORM_WINDOWS
+            )
+        elseif(APPLE)
+            target_compile_definitions(${target_name}
+                PUBLIC
+                    TBX_PLATFORM_MACOS
+            )
+        elseif(UNIX)
+            target_compile_definitions(${target_name}
+                PUBLIC
+                    TBX_PLATFORM_LINUX
+            )
+        endif()
     endif()
 
-    if(TBX_BUILD_SHARED)
-        add_library(${target_name} SHARED)
-        target_compile_definitions(${target_name}
-            PUBLIC
-                TBX_SHARED_LIB
-            PRIVATE
-                TBX_EXPORTING_SYMBOLS
-        )
-    else()
-        add_library(${target_name} STATIC)
-    endif()
-    
-    target_precompile_headers(${target_name} PRIVATE "${PROJECT_SOURCE_DIR}/modules/common/include/tbx/pch.h")
+    file(GLOB_RECURSE MODULE_HEADERS CONFIGURE_DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/include/*.h")
+    file(GLOB_RECURSE MODULE_SOURCES CONFIGURE_DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp")
+    target_sources(${target_name} PRIVATE ${MODULE_SOURCES} ${MODULE_HEADERS})
 
-    if(WIN32)
-        target_compile_definitions(${target_name}
-            PUBLIC
-                TBX_PLATFORM_WINDOWS
-        )
-    elseif(APPLE)
-        target_compile_definitions(${target_name}
-            PUBLIC
-                TBX_PLATFORM_MACOS
-        )
-    elseif(UNIX)
-        target_compile_definitions(${target_name}
-            PUBLIC
-                TBX_PLATFORM_LINUX
-        )
+    target_include_directories(${target_name}
+        PUBLIC
+            $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+            $<INSTALL_INTERFACE:include>
+        PRIVATE
+            ${CMAKE_CURRENT_SOURCE_DIR}
+            ${CMAKE_CURRENT_SOURCE_DIR}/src
+    )
+
+    set(alias_name "Tbx::${target_name}")
+    if(NOT TARGET ${alias_name})
+        add_library(${alias_name} ALIAS ${target_name})
     endif()
 endfunction()
